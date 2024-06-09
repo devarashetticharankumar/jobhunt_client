8ffe94b24d0f46de3313d6ccc25c6b029b2a3661
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-06-09 10:35:24 +0530

    error msg modification

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -104,7 +104,7 @@ const LoginPage = () => {
             />
           </div>
           {error && (
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-1">
+            <div className="text-red-500 text-sm">
               <strong className="font-bold">{error}</strong>
             </div>
           )}
