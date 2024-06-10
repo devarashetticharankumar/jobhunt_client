a5692ea5dbca7858aa04b6d9cd67ac8a60ed7a02
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-06-10 09:18:03 +0530

    changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,9 +67,9 @@ const Navbar = () => {
 
         {/* signup and login btn */}
         <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
-          <Link to="/login" className="py-2 px-5 border rounded">
+          {/* <Link to="/login" className="py-2 px-5 border rounded">
             Log in
-          </Link>
+          </Link> */}
           <Link
             to="/sign-up"
             className="py-2 px-5 border rounded bg-blue text-white"
