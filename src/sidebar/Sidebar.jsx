aaa0539a74e05a1fa6475a1ae1454c7d6677b38a
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-07-31 10:08:30 +0530

    dsfds

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ import EmploymentType from "./EmploymentType";
 const Sidebar = ({ handleChange, handleClick, setJobs }) => {
   return (
     <div className="space-y-5">
-      <h3 className="text-lg font-bold mb-2">Filters</h3>
+      <h3 className="text-2xl font-bold mb-2">Filters</h3>
       <Location handleChange={handleChange} setJobs={setJobs} />
       <Salary handleChange={handleChange} handleClick={handleClick} />
       <JobPostingData handleChange={handleChange} />
