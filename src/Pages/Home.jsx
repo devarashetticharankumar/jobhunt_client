95e9d5ee5414ba26a717756424ca75f6f5a88918
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-08-06 15:55:30 +0530

    adding scroll functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,209 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import Banner from "../components/Banner";
+// import Card from "../components/Card";
+// import Jobs from "./Jobs";
+// import Sidebar from "../sidebar/Sidebar";
+// import NewsLetter from "../components/NewsLetter";
+// import { API_URL } from "../data/apiPath";
+// import Footer from "../components/Footer";
+
+// const Home = () => {
+//   const [selectedCategory, setSelectedCategory] = useState(null);
+//   const [jobs, setJobs] = useState([]);
+//   const [query, setQuery] = useState("");
+//   const [location, setLocation] = useState("");
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const itemsPerPage = 6;
+
+//   useEffect(() => {
+//     setIsLoading(true);
+//     fetch(`${API_URL}/jobs/all-jobs`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setJobs(data);
+//         setIsLoading(false);
+//       });
+//   }, []);
+
+//   // console.log(jobs);
+
+//   // handle input change
+//   const handelInputChange = (event) => {
+//     setQuery(event.target.value);
+//   };
+
+//   // filter jobs by location
+//   const handelSearchByLocation = (event) => {
+//     setLocation(event.target.value);
+//   };
+//   const filteredLocation = jobs.filter(
+//     (locations) =>
+//       locations.jobLocation.toLowerCase().indexOf(location.toLowerCase()) !== -1
+//   );
+
+//   // filter jobs by title
+
+//   const filteredItems = jobs.filter(
+//     (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+//   );
+
+//   // ==============radio filtering =========================
+
+//   const handleChange = (event) => {
+//     setSelectedCategory(event.target.value);
+//   };
+
+//   // ==============button based filtering =========================
+
+//   const handleClick = (event) => {
+//     setSelectedCategory(event.target.value);
+//   };
+
+//   // =============calculate the index range
+
+//   const calculatePageRange = () => {
+//     const startIndex = (currentPage - 1) * itemsPerPage;
+//     const endIndex = startIndex + itemsPerPage;
+//     return { startIndex, endIndex };
+//   };
+
+//   // ========function for the next page
+//   const nextPage = () => {
+//     if (currentPage < Math.ceil(filteredItems.length / itemsPerPage)) {
+//       setCurrentPage(currentPage + 1);
+//     }
+//   };
+
+//   // ========function for the previous page
+//   const prevPage = () => {
+//     if (currentPage > 1) {
+//       setCurrentPage(currentPage - 1);
+//     }
+//   };
+
+//   // ===============main functions===========
+
+//   const filteredData = (jobs, selected, query) => {
+//     let filteredJobs = jobs;
+
+//     // filtering input items
+//     if (query) {
+//       filteredJobs = filteredItems;
+//     }
+
+//     if (location) {
+//       filteredJobs = filteredLocation;
+//     }
+
+//     // category filtering
+
+//     if (selected) {
+//       filteredJobs = filteredJobs.filter(
+//         ({
+//           jobLocation,
+//           maxPrice,
+//           experienceLevel,
+//           salaryType,
+//           employmentType,
+//           postingDate,
+//         }) =>
+//           jobLocation.toLowerCase() === selected.toLowerCase() ||
+//           postingDate >= selected ||
+//           parseInt(maxPrice) <= parseInt(selected) ||
+//           salaryType.toLowerCase() === selected.toLowerCase() ||
+//           experienceLevel.toLowerCase() === selected.toLowerCase() ||
+//           employmentType.toLowerCase() === selected.toLowerCase()
+//       );
+
+//       // console.log(filteredJobs);
+//     }
+
+//     // ========slice the data based on current page===========
+
+//     const { startIndex, endIndex } = calculatePageRange();
+//     filteredJobs = filteredJobs.slice(startIndex, endIndex);
+//     return filteredJobs.map((data, i) => <Card key={i} data={data} />);
+//   };
+
+//   const result = filteredData(jobs, selectedCategory, query);
+
+//   return (
+//     <div>
+//       <Banner
+//         query={query}
+//         handelInputChange={handelInputChange}
+//         location={location}
+//         handelSearchByLocation={handelSearchByLocation}
+//       />
+
+//       {/* main content */}
+//       <div className="bg-[#FAFAFa] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
+//         {/* Left side */}
+//         <div className="bg-white p-4 rounded">
+//           <Sidebar
+//             handleChange={handleChange}
+//             handleClick={handleClick}
+//             setJobs={jobs}
+//           />
+//         </div>
+
+//         {/* job cards */}
+//         <div className="col-span-2 bg-white">
+//           {isLoading ? (
+//             <p className="font-medium flex items-center justify-center mt-16">
+//               Loading...
+//             </p>
+//           ) : result.length > 0 ? (
+//             <Jobs result={result} />
+//           ) : (
+//             <>
+//               <h3 className="text-lg font-bold mb-2">{result.length} jobs</h3>
+//               <p>No data found.</p>
+//             </>
+//           )}
+
+//           {/*  pagination*/}
+//           {result.length > 0 ? (
+//             <div className="flex justify-center mt-4 space-x-8">
+//               <button
+//                 onClick={prevPage}
+//                 disabled={currentPage === 1}
+//                 className="hover:underline"
+//               >
+//                 Previous
+//               </button>
+//               <span className="mx-2">
+//                 page {currentPage} of{" "}
+//                 {Math.ceil(filteredItems.length / itemsPerPage)}
+//               </span>
+//               <button
+//                 onClick={nextPage}
+//                 disabled={
+//                   currentPage === Math.ceil(filteredItems.length / itemsPerPage)
+//                 }
+//                 className="hover:underline"
+//               >
+//                 Next
+//               </button>
+//             </div>
+//           ) : (
+//             ""
+//           )}
+//         </div>
+
+//         {/* Right side */}
+//         <div className="bg-white p-4 rounded">
+//           <NewsLetter />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import { useEffect, useRef, useState } from "react";
 import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Jobs from "./Jobs";
@@ -16,6 +221,8 @@ const Home = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6;
 
+  const jobsRef = useRef(null);
+
   useEffect(() => {
     setIsLoading(true);
     fetch(`${API_URL}/jobs/all-jobs`)
@@ -26,41 +233,39 @@ const Home = () => {
       });
   }, []);
 
-  // console.log(jobs);
-
-  // handle input change
-  const handelInputChange = (event) => {
+  const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  // filter jobs by location
-  const handelSearchByLocation = (event) => {
+  const handleSearchByLocation = (event) => {
     setLocation(event.target.value);
   };
-  const filteredLocation = jobs.filter(
-    (locations) =>
-      locations.jobLocation.toLowerCase().indexOf(location.toLowerCase()) !== -1
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setCurrentPage(1); // Reset to the first page on search
+
+    // Scroll to the jobs section
+    if (jobsRef.current) {
+      jobsRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const filteredLocation = jobs.filter((job) =>
+    job.jobLocation.toLowerCase().includes(location.toLowerCase())
   );
 
-  // filter jobs by title
-
-  const filteredItems = jobs.filter(
-    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  const filteredItems = jobs.filter((job) =>
+    job.jobTitle.toLowerCase().includes(query.toLowerCase())
   );
-
-  // ==============radio filtering =========================
 
   const handleChange = (event) => {
     setSelectedCategory(event.target.value);
   };
 
-  // ==============button based filtering =========================
-
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
   };
-
-  // =============calculate the index range
 
   const calculatePageRange = () => {
     const startIndex = (currentPage - 1) * itemsPerPage;
@@ -68,26 +273,21 @@ const Home = () => {
     return { startIndex, endIndex };
   };
 
-  // ========function for the next page
   const nextPage = () => {
     if (currentPage < Math.ceil(filteredItems.length / itemsPerPage)) {
       setCurrentPage(currentPage + 1);
     }
   };
 
-  // ========function for the previous page
   const prevPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   };
 
-  // ===============main functions===========
-
-  const filteredData = (jobs, selected, query) => {
+  const filteredData = () => {
     let filteredJobs = jobs;
 
-    // filtering input items
     if (query) {
       filteredJobs = filteredItems;
     }
@@ -96,9 +296,7 @@ const Home = () => {
       filteredJobs = filteredLocation;
     }
 
-    // category filtering
-
-    if (selected) {
+    if (selectedCategory) {
       filteredJobs = filteredJobs.filter(
         ({
           jobLocation,
@@ -108,38 +306,33 @@ const Home = () => {
           employmentType,
           postingDate,
         }) =>
-          jobLocation.toLowerCase() === selected.toLowerCase() ||
-          postingDate >= selected ||
-          parseInt(maxPrice) <= parseInt(selected) ||
-          salaryType.toLowerCase() === selected.toLowerCase() ||
-          experienceLevel.toLowerCase() === selected.toLowerCase() ||
-          employmentType.toLowerCase() === selected.toLowerCase()
+          jobLocation.toLowerCase() === selectedCategory.toLowerCase() ||
+          postingDate >= selectedCategory ||
+          parseInt(maxPrice) <= parseInt(selectedCategory) ||
+          salaryType.toLowerCase() === selectedCategory.toLowerCase() ||
+          experienceLevel.toLowerCase() === selectedCategory.toLowerCase() ||
+          employmentType.toLowerCase() === selectedCategory.toLowerCase()
       );
-
-      // console.log(filteredJobs);
     }
-
-    // ========slice the data based on current page===========
 
     const { startIndex, endIndex } = calculatePageRange();
     filteredJobs = filteredJobs.slice(startIndex, endIndex);
     return filteredJobs.map((data, i) => <Card key={i} data={data} />);
   };
 
-  const result = filteredData(jobs, selectedCategory, query);
+  const result = filteredData();
 
   return (
     <div>
       <Banner
         query={query}
-        handelInputChange={handelInputChange}
+        handleInputChange={handleInputChange}
         location={location}
-        handelSearchByLocation={handelSearchByLocation}
+        handleSearchByLocation={handleSearchByLocation}
+        handleSearch={handleSearch}
       />
 
-      {/* main content */}
       <div className="bg-[#FAFAFa] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
-        {/* Left side */}
         <div className="bg-white p-4 rounded">
           <Sidebar
             handleChange={handleChange}
@@ -148,8 +341,7 @@ const Home = () => {
           />
         </div>
 
-        {/* job cards */}
-        <div className="col-span-2 bg-white">
+        <div className="col-span-2 bg-white" ref={jobsRef}>
           {isLoading ? (
             <p className="font-medium flex items-center justify-center mt-16">
               Loading...
@@ -163,8 +355,7 @@ const Home = () => {
             </>
           )}
 
-          {/*  pagination*/}
-          {result.length > 0 ? (
+          {result.length > 0 && (
             <div className="flex justify-center mt-4 space-x-8">
               <button
                 onClick={prevPage}
@@ -187,12 +378,9 @@ const Home = () => {
                 Next
               </button>
             </div>
-          ) : (
-            ""
           )}
         </div>
 
-        {/* Right side */}
         <div className="bg-white p-4 rounded">
           <NewsLetter />
         </div>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,37 +1,101 @@
+// /* eslint-disable react/prop-types */
+// import React, { useEffect, useState } from "react";
+// import { FiMapPin, FiSearch } from "react-icons/fi";
+// import { API_URL } from "../data/apiPath";
+
+// const Banner = ({
+//   query,
+//   handelInputChange,
+//   location,
+//   handelSearchByLocation,
+// }) => {
+//   const [jobs, setJobs] = useState([]);
+//   const [searchText, setSearchText] = useState("");
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     setIsLoading(true);
+//     fetch(`${API_URL}/jobs/all-jobs`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setJobs(data);
+//         setIsLoading(false);
+//       });
+//   }, [searchText]);
+
+//   const handleSearch = () => {
+//     const filter = jobs.filter(
+//       (job) =>
+//         job.jobTitle.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+//     );
+//     setJobs(filter);
+//   };
+//   return (
+//     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20">
+//       <h1 className="text-5xl font-bold text-primary">
+//         Find your <span className="text-blue">new job</span> today
+//       </h1>
+//       <p className="text-lg text-black/70 mb-8">
+//         Thousands of jobs in the computer, engineering and technology sectors
+//         are waiting for you.
+//       </p>
+
+//       <form>
+//         <div className="flex justify-start md:flex-row flex-col md:gap-0 gap-4">
+//           <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full">
+//             <input
+//               type="text"
+//               name="title"
+//               id="title"
+//               value={query}
+//               onChange={handelInputChange}
+//               // onChange={(e) => setSearchText(e.target.value)}
+//               placeholder="what position are you looking for?"
+//               className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6"
+//             />
+//             <FiSearch className="absolute mt-2.5 ml-2 text-gray-400" />
+//           </div>
+//           <div className="flex md:rounded-s-none rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3 w-full">
+//             <input
+//               type="text"
+//               name="title"
+//               id="title"
+//               value={location}
+//               onChange={handelSearchByLocation}
+//               placeholder="Location"
+//               className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6"
+//             />
+//             <FiMapPin className="absolute mt-2.5 ml-2 text-gray-400" />
+//           </div>
+//           <button
+//             type="submit"
+//             className="bg-blue hover:bg-indigo-700 py-2 px-8 text-white md:rounded-s-none rounded"
+//             onClick={handleSearch}
+//           >
+//             Search
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+import React from "react";
 import { FiMapPin, FiSearch } from "react-icons/fi";
-import { API_URL } from "../data/apiPath";
 
 const Banner = ({
   query,
-  handelInputChange,
+  handleInputChange,
   location,
-  handelSearchByLocation,
+  handleSearchByLocation,
+  handleSearch,
 }) => {
-  const [jobs, setJobs] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(`${API_URL}/jobs/all-jobs`)
-      .then((res) => res.json())
-      .then((data) => {
-        setJobs(data);
-        setIsLoading(false);
-      });
-  }, [searchText]);
-
-  const handleSearch = () => {
-    const filter = jobs.filter(
-      (job) =>
-        job.jobTitle.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
-    );
-    setJobs(filter);
-  };
   return (
-    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py-14">
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20">
       <h1 className="text-5xl font-bold text-primary">
         Find your <span className="text-blue">new job</span> today
       </h1>
@@ -40,7 +104,7 @@ const Banner = ({
         are waiting for you.
       </p>
 
-      <form>
+      <form onSubmit={handleSearch}>
         <div className="flex justify-start md:flex-row flex-col md:gap-0 gap-4">
           <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full">
             <input
@@ -48,9 +112,8 @@ const Banner = ({
               name="title"
               id="title"
               value={query}
-              onChange={handelInputChange}
-              // onChange={(e) => setSearchText(e.target.value)}
-              placeholder="what position are you looking for?"
+              onChange={handleInputChange}
+              placeholder="What position are you looking for?"
               className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6"
             />
             <FiSearch className="absolute mt-2.5 ml-2 text-gray-400" />
@@ -58,10 +121,10 @@ const Banner = ({
           <div className="flex md:rounded-s-none rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3 w-full">
             <input
               type="text"
-              name="title"
-              id="title"
+              name="location"
+              id="location"
               value={location}
-              onChange={handelSearchByLocation}
+              onChange={handleSearchByLocation}
               placeholder="Location"
               className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6"
             />
@@ -70,7 +133,6 @@ const Banner = ({
           <button
             type="submit"
             className="bg-blue hover:bg-indigo-700 py-2 px-8 text-white md:rounded-s-none rounded"
-            onClick={handleSearch}
           >
             Search
           </button>
