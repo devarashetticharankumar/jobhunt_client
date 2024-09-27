691fe24ad931694e9ee14c0b809f99d53352a70d
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-09-27 15:22:17 +0530

    adding google analytics code on index.html file in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag("config", "G-XQVWKXQYC9", {
+      page_path: location.pathname,
+    });
+  }, [location]);
   return (
     <>
       <Navbar />
