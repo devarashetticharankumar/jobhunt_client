e02e5d1fe072f037f4deed5f11b5a7708afea1a4
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-10-05 15:38:01 +0530

    adding infeedAd component on salary page component

--- src/components/InFeedAd.jsx
+++ src/components/InFeedAd.jsx
@@ -16,14 +16,6 @@ const InFeedAd = () => {
       className="in-feed-ad my-4 mx-5
     "
     >
-      {/* <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-format="fluid"
-        data-ad-layout-key="-ff-26-6o-g1+1xb"
-        data-ad-client="ca-pub-8430285426081478"
-        data-ad-slot="9720225381"
-      ></ins> */}
       <ins
         className="adsbygoogle"
         style={{ display: "block" }}
