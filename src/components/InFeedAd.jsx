96c218fb10f8893b9f197e79df24d6f3c61f1f05
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-10-05 14:49:49 +0530

    dsvwd

--- src/components/InFeedAd.jsx
+++ src/components/InFeedAd.jsx
@@ -12,7 +12,7 @@ const InFeedAd = () => {
   }, []);
 
   return (
-    <div className="in-feed-ad my-4">
+    <div className="in-feed-ad my-4 mx-4">
       {/* <ins
         className="adsbygoogle"
         style={{ display: "block" }}
