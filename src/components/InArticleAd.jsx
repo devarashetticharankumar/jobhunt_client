dadf02bb4930763a5d356660fa099668f8e87e7c
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-10-05 15:08:19 +0530

    adding inArticalAd component in jobDetails component

---
+++ src/components/InArticleAd.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+const InFeedAd = () => {
+  useEffect(() => {
+    try {
+      if (window.adsbygoogle) {
+        window.adsbygoogle.push({});
+      }
+    } catch (e) {
+      console.error("Adsense error:", e);
+    }
+  }, []);
+
+  return (
+    <div
+      className="in-feed-ad my-4 mx-5
+    "
+    >
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-8430285426081478"
+        data-ad-slot="9016216095"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
+  );
+};
+
+export default InFeedAd;
