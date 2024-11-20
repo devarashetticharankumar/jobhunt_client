e6e4c510d21260ceeea3be8b08c03905700f1841
devarashetticharankumar <devarashetticharankumar2580@gmail.com>
2024-11-20 15:17:03 +0530

    change api_url

--- src/data/apiPath.js
+++ src/data/apiPath.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:5001";
 
-export const API_URL = "https://jobhunt-backend.onrender.com";
+export const API_URL = "https://jobhunt-backend-1.onrender.com";
