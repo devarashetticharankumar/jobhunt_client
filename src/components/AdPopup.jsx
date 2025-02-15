fc6823d5c998ba7199088eaa17c1c80af9a83676
devarashetticharankumar <149133505+devarashetticharankumar@users.noreply.github.com>
2025-02-16 04:45:08 +0530

    Update AdPopup.jsx

--- src/components/AdPopup.jsx
+++ src/components/AdPopup.jsx
@@ -69,7 +69,7 @@ const AdPopup = () => {
                 height: "200px",
               }}
               data-ad-client="ca-pub-8430285426081478"
-              data-ad-slot="2195791299"
+              data-ad-slot="9016216095"
               data-ad-format="auto"
               data-full-width-responsive="true"
             ></ins>
